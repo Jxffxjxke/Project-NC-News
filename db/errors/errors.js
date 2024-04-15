3479c0b4ccbfe6368fd7b28cf1bdeb2f646368ba
Jxffxjxke <jaffajake25@gmail.com>
2024-04-15 16:55:25 +0100

    Task 5 - get articles

--- db/errors/errors.js
+++ db/errors/errors.js
@@ -2,8 +2,10 @@ exports.sendURLError = (req, res) => {
   res.status(404).send({ message: "URL not found" });
 };
 
-exports.sendMadeError = (err, req, res, next) => {
+exports.sendError = (err, req, res, next) => {
   if (err.status && err.message) {
     res.status(err.status).send({ message: err.message });
+  } else if (err.code === "22P02") {
+    res.status(400).send({ message: "Bad request" });
   }
 };
