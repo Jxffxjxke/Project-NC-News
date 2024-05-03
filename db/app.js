5bc43f314eb0b635f028b5d6a9578a6158746b18
Jxffxjxke <jaffajake25@gmail.com>
2024-05-03 16:52:03 +0100

    cross origin resourse sharing

--- db/app.js
+++ db/app.js
@@ -2,6 +2,9 @@ const { sendURLError, sendError } = require("../errors");
 const express = require("express");
 const app = express();
 const apiRouter = require("../routes/api-router");
+const cors = require( "cors" );
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", apiRouter);
